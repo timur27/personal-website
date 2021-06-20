b8fa5d9ecf2e5d0e09eddb527f7760eb3496215e
Timur <timur@macbook-pro-timur.home>
2021-06-20 15:11:04 +0200

    [nav] Add dynamic redirection without refreshing the page

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,5 +1,7 @@
-
-const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+const navigateTo = url => {
+    history.pushState(null, null, url); 
+    router(); 
+};
 
 const router = async () => {
     const routes = [
@@ -16,9 +18,23 @@ const router = async () => {
         };
     });
 
-    console.log(potentialMatches);
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch); 
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+    console.log(match.route.view());
 };
 
 document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault(); 
+            navigateTo(e.target.href);
+        }
+    });
+    
     router(); 
 })
