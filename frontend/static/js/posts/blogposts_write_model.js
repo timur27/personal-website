295e33a21612701731af880b32dc0b286c17a586
timurk <timur.karimow.95@gmail.com>
2021-07-10 19:07:28 +0200

    Cleanup

--- frontend/static/js/posts/blogposts_write_model.js
+++
@@ -1,9 +0,0 @@
-import * as MyFn from "./blogposts_persistence.js"
-
-document.getElementById("add-blog-post-form")
-    .addEventListener("submit", function(e){
-        var title = document.getElementById("blogPostTitle").value;
-        var desc = document.getElementById("blogPostContent").value;
-
-        MyFn.addPost(title, desc); 
-    });
